c67b622069cfd48839c15ab1a01fd9697045209e
momoclouq <57221289+momoclouq@users.noreply.github.com>
2020-07-01 09:19:19 +0700

    Create start.py

---
+++ Tutorial/Tutorial1/start.py
@@ -0,0 +1,19 @@
+print("hello world")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+#first task
+print("Num1 is ", num1)
+print("Num2 is ", num2)
+
+#second task
+if (num1 > num2):
+    print("num1 is larger: ", num1)
+else:
+    print("num2 is larger: ", num2)
+
+#third task
+print("Rectangle area: ", num1, " * ",num2, " = ", num1 * num2)
+
+#fourth task
+print(num1, " ^ ", num2, " = ", (num1 ** num2))
